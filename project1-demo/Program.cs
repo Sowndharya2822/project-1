ecfb637fa2970071916acb241bd8182af6f299c2
Sowndharya2822 <sowndharyas199@gmail.com>
2024-07-23 12:53:01 +0530

    new file added

---
+++ project1-demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            q2();
+        }
+        public static void q1()
+        {
+            Console.WriteLine("enter the number1");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the numbeer2");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(number1+number2);
+            Console.WriteLine(number1-number2);
+            Console.WriteLine(number1*number2);
+        }
+        public static void q2()
+        {
+            Console.WriteLine("enter the value1");
+            float value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the power");
+            float power = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow(value1,power));
+        }
+
+
+    }
+}
